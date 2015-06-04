6e95b55285109af0a2c14c466d853df886026fb3
balaji vijayan <balajivjn20@gmail.com>
2015-06-04 18:20:51 +0530

    add project 2_12

---
+++ 2_12_AOP/src/main/java/au/com/attra/aop/ArithmeticCalculator.java
@@ -0,0 +1,11 @@
+package au.com.attra.aop;
+
+public interface ArithmeticCalculator {
+	public double add(double a, double b);
+
+	public double sub(double a, double b);
+
+	public double mul(double a, double b);
+
+	public double div(double a, double b);
+}
---
+++ 2_12_AOP/src/main/java/au/com/attra/aop/UnitCalculator.java
@@ -0,0 +1,7 @@
+package au.com.attra.aop;
+
+public interface UnitCalculator {
+	public double kilogramToPound(double kilogram);
+
+	public double kilometerToMile(double kilometer);
+}
